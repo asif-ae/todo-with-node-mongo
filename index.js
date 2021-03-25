5af5d2e90198652bde35b271f539073697b69e37
Asif Estiak <me.asif.ae@gmail.com>
2021-03-25 16:36:35 +0600

    initialize project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://tushartd:hxhxsrRJooXPNG$R@cluster0.lq9rh.mongodb.net/tasks?retryWrites=true&w=majority";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productCollection = client.db("organicdb").collection("open");
+
+  // Read something from the database
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray( (err, documents) => {
+      res.send(documents);
+    } )
+  })
+});
+
+app.listen(3000);
